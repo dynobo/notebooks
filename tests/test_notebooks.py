c34ddcf151446ae8d405fbacb5bd05ab15856c90
dynobo <dynobo@mailbox.org>
2020-03-22 20:52:52 +0100

    Add comment; Clean up

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -3,26 +3,41 @@ import sys
 import logging
 
 # Extra
-from nbconvert import NotebookExporter
 from nbconvert.preprocessors import ExecutePreprocessor, CellExecutionError
 import nbformat
-
-# Own
-from .context import notebooks
 
 LOGGER = logging.getLogger(__name__)
 
 
-def notebook_run(path):
-    with open(path, "r") as f:
+def log_error_list(errors):
+    for idx, error in enumerate(errors):
+        LOGGER.error("=" * 60)
+        LOGGER.error(f"ERROR NO. {idx+1}:")
+        for k, v in error.items():
+            if k == "traceback":
+                LOGGER.error(f"{k}:")
+                sys.stdout.write("\n".join(v))
+            else:
+                LOGGER.error(f"{k:<12}: {v}")
+
+
+def notebook_run(nb_file):
+    """Load an ipynb file, execute it, and log traceback in case of error.
+
+    Arguments:
+        nb_file {str} -- Path to ipynb file
+
+    Returns:
+        tuple ({Notebook}, {list}) -- Executed Notebook object and list of errors
+    """
+    with open(nb_file, "r") as f:
         nb = nbformat.read(f, as_version=4)
 
     try:
         ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
-        out = ep.preprocess(nb, {"metadata": {"path": "notebooks/"}})
+        ep.preprocess(nb, {"metadata": {"path": "notebooks/"}})
     except CellExecutionError:
-        out = None
-        print(f"Error while executing notebook")
+        LOGGER.error(f"Error while executing notebook!")
         raise
     finally:
         errors = [
@@ -32,20 +47,17 @@ def notebook_run(path):
             for output in cell["outputs"]
             if output.output_type == "error"
         ]
-        # print(f"Errors while executing notebook {path}: \n", errors)
-        for idx, error in enumerate(errors):
-            LOGGER.error("=" * 60)
-            LOGGER.error(f"ERROR NO. {idx+1}:")
-            for k, v in error.items():
-                if k == "traceback":
-                    LOGGER.error(f"{k}:")
-                    sys.stdout.write("\n".join(v))
-                else:
-                    LOGGER.error(f"{k:<12}: {v}")
+        log_error_list(errors)
+
     return nb, errors
 
 
 def test_execute_notebooks(nb_files):
+    """Test list of ipynb-files get executed without errors
+
+    Arguments:
+        nb_files {list} -- List of Paths of ipynb to test
+    """
     for nb_file in nb_files:
         LOGGER.info("Testing '" + nb_file + "' ...")
         nb, errors = notebook_run(nb_file)
