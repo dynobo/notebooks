2b43c762bac5dc43e5ede71cc12b93dd8f16f1cb
dynobo <dynobo@mailbox.org>
2020-03-23 08:22:09 +0100

    Exclude tetris from test

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,5 +7,8 @@ LOGGER = logging.getLogger(__name__)
 @pytest.fixture(scope="function")
 def nb_files():
     prefix = "notebooks/"
-    notebooks = ["2020-02-Alexa.ipynb", "2020-Tetris.ipynb"]
+    notebooks = [
+        "2020-02-Alexa.ipynb",
+        # "2020-Tetris.ipynb"
+    ]
     return [prefix + nb for nb in notebooks]
