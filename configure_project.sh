2c0c9fde5490ceca20f54d59a1e635b342d29704
dynobo <dynobo@mailbox.org>
2020-04-03 10:21:59 +0200

    Remove unnecessary whitespace

--- configure_project.sh
+++ configure_project.sh
@@ -1,16 +1,13 @@
 for ARGUMENT in "$@"
 do
-
     KEY=$(echo $ARGUMENT | cut -f1 -d=)
     VALUE=$(echo $ARGUMENT | cut -f2 -d=)   
 
     case "$KEY" in
             MODULE)      MODULE=${VALUE} ;;
             REGISTRY)    REGISTRY=${VALUE} ;;
-            *)   
-    esac    
-
-
+            *)
+    esac
 done
 
 DUMMY_MODULE='blueprint'
